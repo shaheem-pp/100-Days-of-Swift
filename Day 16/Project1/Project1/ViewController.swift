9129649a7d5ccc2d08a1541f8f5e6f76afa97c5c
the-codeholic <shanofficial2000@gmail.com>
2022-05-25 11:36:20 +0530

    Day 16

---
+++ Day 16/Project1/Project1/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Shaheem PP on 25/05/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory (atPath: path)
+        for item in items{
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+            print(item)
+        }
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+}
