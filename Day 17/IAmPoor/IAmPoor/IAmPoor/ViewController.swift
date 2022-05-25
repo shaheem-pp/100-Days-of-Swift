ea22c5fb316cb3e0100bb2edb03e0f4e4b5eef9f
the-codeholic <shanofficial2000@gmail.com>
2022-05-26 00:33:22 +0530

    Day 17

---
+++ Day 17/IAmPoor/IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Shaheem PP on 26/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
