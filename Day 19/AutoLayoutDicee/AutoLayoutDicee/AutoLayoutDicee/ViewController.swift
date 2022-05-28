23644009e17bb47389ed5529c574846c18e47ae7
the-codeholic <shanofficial2000@gmail.com>
2022-05-28 20:04:52 +0530

    Day 19

---
+++ Day 19/AutoLayoutDicee/AutoLayoutDicee/AutoLayoutDicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout-iOS13
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+     
+     @IBAction func rollButtonPressed(_ sender: UIButton) {
+         
+         let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+         
+         diceImageView1.image = diceArray[Int.random(in: 0...5)]
+         diceImageView2.image = diceArray[Int.random(in: 0...5)]
+         
+     }
+}
+
