0d7616f1063ca12d917731515aea6ddf8a7ecfa8
the-codeholic <shanofficial2000@gmail.com>
2022-05-30 19:53:38 +0530

    Day 21

---
+++ Day 21/Zakat Calculator/Zakat Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zakat Calculator
+//
+//  Created by Shaheem PP on 30/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Day 21/Zakat Calculator/Zakat CalculatorUITests/Zakat_CalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Zakat_CalculatorUITestsLaunchTests.swift
+//  Zakat CalculatorUITests
+//
+//  Created by Shaheem PP on 30/05/22.
+//
+
+import XCTest
+
+class Zakat_CalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
