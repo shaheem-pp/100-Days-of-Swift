c74106d8b3f9093d9641c2150b6b4ca36c5d794e
Shaheem PP <shanofficial2000@gmail.com>
2022-06-22 19:26:27 +0530

    Day 42-43

---
+++ Day42-43/Weatherly/Weatherly/Control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weatherly
+//
+//  Created by Shaheem PP on 22/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var currunteLocBtn: UIButton!
+    @IBOutlet weak var searchBtn: UIButton!
+    @IBOutlet weak var locationTextView: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
