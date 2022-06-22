ea6890a1219b79268ddad8ecff4c8cdc242ab17f
Shaheem PP <shanofficial2000@gmail.com>
2022-06-22 22:43:55 +0530

    Day 42

--- Day42-43/Weatherly/Weatherly/Control/ViewController.swift
+++ Day42-43/Weatherly/Weatherly/Control/ViewController.swift
@@ -7,19 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var weatherImage: UIImageView!
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var currentLocation: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
     @IBOutlet weak var temperatureLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
-    @IBOutlet weak var currunteLocBtn: UIButton!
-    @IBOutlet weak var searchBtn: UIButton!
-    @IBOutlet weak var locationTextView: UITextField!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate=self
+        
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        locationLabel.text = searchTextField.text
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField.text==""{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    
+    
 }
 
