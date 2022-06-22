79ca2d00838c1f47967b58867470cb6cc36a83ae
Shaheem PP <shanofficial2000@gmail.com>
2022-06-22 22:53:54 +0530

    Day 43

---
+++ Day42-43/RandomNumberChoose/RandomNumberChoose/RandomNumberChooseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberChooseApp.swift
+//  RandomNumberChoose
+//
+//  Created by Shaheem PP on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberChooseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
