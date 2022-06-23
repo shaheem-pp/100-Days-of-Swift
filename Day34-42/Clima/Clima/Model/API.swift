ccaaf58dcd67269e266265e354b8de0578031f0b
Shaheem PP <shanofficial2000@gmail.com>
2022-06-23 13:15:55 +0530

    Day 45

---
+++ Day34-42/Clima/Clima/Model/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  Clima
+//
+//  Created by Shaheem PP on 21/06/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct APIKEY{
+    let myApiKey = "ceb9aa4cba83b30679f4ab2fe2409a94"
+}
