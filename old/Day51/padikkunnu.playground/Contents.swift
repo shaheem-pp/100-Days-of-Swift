d98e9ef3a7715fdf411e9d5b17e2fe015051969c
Shaheem PP <shanofficial2000@gmail.com>
2023-05-26 17:19:20 +0530

    26/05/2023

---
+++ old/Day51/padikkunnu.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print(greeting)
+
+
+struct Toys{
+    var name : String
+    var numbers : Int
+    var paintingAbility: (() -> Void)?
+}
+
+let car = Toys(name: "MacQueen", numbers: 2, paintingAbility: nil)
+let giraffe = Toys(name: "Tess", numbers: 1, paintingAbility: {print("Tess can Paint👩🏻‍🎨")})
+
+
+func sayHello(){
+    print("Hello")
+}
+
+sayHello()
+sayHello()
+sayHello()
+
+func sum(num1:Int,num2:Int)->Int{
+    return num1+num2
+}
+
+var value = sum(num1: 10, num2: 10)
+print(value)
+
+class Chef {
+    var name: String = ""
+}
+
+class myChef: Chef {
+    func cook(){
+        print("Your chef is cooking")
+    }
+}
+
+var me = myChef()
+me.name="Hello"
+print(me.name)
+me.cook()
