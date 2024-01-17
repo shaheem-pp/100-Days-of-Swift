b2e3baa65ebf6db824b72b584757a6a1fbebda36
shaheem-pp <shanofficial2000@gmail.com>
2024-01-17 21:15:49 +0530

    WhatsSpp Clone

---
+++ profileShow/profileShow/profileShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileShowApp.swift
+//  profileShow
+//
+//  Created by Shaheem PP on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct profileShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ whatsapp/whatsapp/whatsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsappApp.swift
+//  whatsapp
+//
+//  Created by Shaheem PP on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct whatsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
